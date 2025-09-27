1bea408c2319fdbbdb8eba6967245c0b764455f8
Monisha Ishwar <monishamoger15@gmail.com>
2025-09-27 19:13:11 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace WindowsApp_Registration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAccNo = new System.Windows.Forms.Label();
+            this.lblACC = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.lblAccounts = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.cmdAccounts = new System.Windows.Forms.ComboBox();
+            this.cbStatus = new System.Windows.Forms.CheckBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblAccNo
+            // 
+            this.lblAccNo.AutoSize = true;
+            this.lblAccNo.Location = new System.Drawing.Point(189, 69);
+            this.lblAccNo.Name = "lblAccNo";
+            this.lblAccNo.Size = new System.Drawing.Size(44, 16);
+            this.lblAccNo.TabIndex = 0;
+            this.lblAccNo.Text = "label1";
+            // 
+            // lblACC
+            // 
+            this.lblACC.AutoSize = true;
+            this.lblACC.Location = new System.Drawing.Point(72, 69);
+            this.lblACC.Name = "lblACC";
+            this.lblACC.Size = new System.Drawing.Size(106, 16);
+            this.lblACC.TabIndex = 1;
+            this.lblACC.Text = "Account Number";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(72, 129);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(47, 16);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "Name\'";
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Location = new System.Drawing.Point(72, 185);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(58, 16);
+            this.lblAddress.TabIndex = 3;
+            this.lblAddress.Text = "Address";
+            // 
+            // lblAccounts
+            // 
+            this.lblAccounts.AutoSize = true;
+            this.lblAccounts.Location = new System.Drawing.Point(72, 246);
+            this.lblAccounts.Name = "lblAccounts";
+            this.lblAccounts.Size = new System.Drawing.Size(62, 16);
+            this.lblAccounts.TabIndex = 4;
+            this.lblAccounts.Text = "Accounts";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(72, 312);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(44, 16);
+            this.lblStatus.TabIndex = 5;
+            this.lblStatus.Text = "Status";
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.Location = new System.Drawing.Point(72, 366);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(44, 16);
+            this.lblMsg.TabIndex = 6;
+            this.lblMsg.Text = "label7";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(224, 123);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(276, 22);
+            this.txtName.TabIndex = 7;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(224, 179);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(276, 22);
+            this.txtAddress.TabIndex = 8;
+            // 
+            // cmdAccounts
+            // 
+            this.cmdAccounts.FormattingEnabled = true;
+            this.cmdAccounts.Location = new System.Drawing.Point(224, 246);
+            this.cmdAccounts.Name = "cmdAccounts";
+            this.cmdAccounts.Size = new System.Drawing.Size(121, 24);
+            this.cmdAccounts.TabIndex = 9;
+            // 
+            // cbStatus
+            // 
+            this.cbStatus.AutoSize = true;
+            this.cbStatus.Location = new System.Drawing.Point(224, 308);
+            this.cbStatus.Name = "cbStatus";
+            this.cbStatus.Size = new System.Drawing.Size(81, 20);
+            this.cbStatus.TabIndex = 10;
+            this.cbStatus.Text = "cbStatus";
+            this.cbStatus.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(224, 453);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 11;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(425, 453);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 12;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(708, 539);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.cbStatus);
+            this.Controls.Add(this.cmdAccounts);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblMsg);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblAccounts);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblACC);
+            this.Controls.Add(this.lblAccNo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAccNo;
+        private System.Windows.Forms.Label lblACC;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.Label lblAccounts;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblMsg;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.ComboBox cmdAccounts;
+        private System.Windows.Forms.CheckBox cbStatus;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace WindowsApp_Registration
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection();
+        int status = 1;
+        public Form1()
+        {
+            InitializeComponent();
+            con.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=Banking; Integrated Security=true";
+            lblAccNo.Text = AccountNumber().ToString();
+            cmdAccounts.DataSource = Accounts();
+            cmdAccounts.DisplayMember = "Account";
+            cmdAccounts.ValueMember = "Id";
+            cmdAccounts.SelectedIndex = 0;
+            
+        }
+        int AccountNumber()
+        {
+            SqlCommand cmd = new SqlCommand("select max(Accno)+1 from Customers ", con);
+            if (con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            return Convert.ToInt32(cmd.ExecuteScalar());
+        }
+
+        DataTable Accounts()
+        {
+            SqlDataAdapter da = new SqlDataAdapter("select Id, Account from Accounts ", con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            DataRow r = dt.NewRow();
+            r[0] = -1;
+            r[1] = "choose accounts";
+            dt.Rows.InsertAt(r, 0);
+            return dt;
+
+        }
+        
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if(!cbStatus.Checked)
+            {
+                status = 0;
+            }
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            if(con.State==ConnectionState.Closed)
+            {
+                con.Open();
+
+            }
+            cmd.CommandText="insert into Customers values(@accno, @name, @address, @account, @status)";
+            cmd.Parameters.AddWithValue("@accno", Convert.ToInt32(lblAccNo.Text));
+
+            cmd.Parameters.AddWithValue("@name", txtName.Text.Trim());
+            cmd.Parameters.AddWithValue("@address", txtAddress.Text.Trim());
+            cmd.Parameters.AddWithValue("@account", Convert.ToInt32(cmdAccounts.SelectedValue));
+            cmd.Parameters.AddWithValue("@status", status);
+
+            int updated = cmd.ExecuteNonQuery();
+            if(updated>0)
+            {
+                lblMsg.Text = "Inseted Suceesfully";
+            }
+            else
+            {
+                lblMsg.Text = "sorry we cant insert";
+            }
+            con.Close();
+
+
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            txtName.Clear();
+            txtAddress.Clear();
+            lblAccNo.Text = AccountNumber().ToString();
+            cmdAccounts.SelectedIndex = 0;
+            cbStatus.Checked = true;
+        }
+        
+
+    }
+    
+}
